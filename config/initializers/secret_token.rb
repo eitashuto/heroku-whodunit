a2fa842a996a97d7dec20bc8f0bf3ca8b73cb0b3
Eita Shuto <eita.shuto@toshiba.co.jp>
2013-05-15 16:16:36 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whodunit::Application.config.secret_token = '763a59e7c2bf1b9343a523717d6e323a982704051450f929e8755e086ccecee673f6ee5288001ac1a5a38ca642356d2801942abea26ce5dd07f2142794155980'
